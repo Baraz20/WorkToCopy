961c056754cfacb3c3de3e26a54a9b4e72f08fe3
Baraz20 <40857935+Baraz20@users.noreply.github.com>
2020-02-04 13:10:39 +0200

    Add files via upload

---
+++ BasicQueue/BasicQueue/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace BasicQueue
+{
+    class Program
+    {
+        public static int SumQ(Queue<int> Q)
+        {
+            Queue<int> temp = Q.CopyQ();
+            int Sum = 0;
+            while (!temp.IsEmpty())
+            {
+                Sum += temp.Remove();
+            }
+            return Sum;
+        }
+        public static int MaxQ(Queue<int> Q)
+        {
+            Queue<int> temp = Q.CopyQ();
+            int Max = temp.Remove();
+            while (!temp.IsEmpty())
+            {
+                if (temp.Head()>Max)
+                {
+                    Max = temp.Head();
+                }
+                temp.Remove();
+            }
+            return Max;
+        }
+        public static Queue<int> EvenCopy(Queue<int> Q)
+        {
+            Queue<int> Temp = Q.CopyQ();
+            Queue<int> Sol = new Queue<int>();
+            while (!Temp.IsEmpty())
+            {
+                if (Temp.Head()%2==0)
+                {
+                    Sol.Insert(Temp.Head());
+                }
+                Temp.Remove();
+            }
+            return Sol;
+        }
+        public static void MoveMax2Last(Queue<int> Q)
+        {
+            Queue<int> temp = new Queue<int>();
+            int Max = MaxQ(Q);
+            while (!Q.IsEmpty())
+            {
+                temp.Insert(Q.Remove());      
+            }
+            while (!temp.IsEmpty())
+            {
+                if (temp.Head()!=Max)
+                {
+                    Q.Insert(temp.Head());
+                }
+                temp .Remove();
+            }
+            Q.Insert(Max);
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> Q = new Queue<int>();
+            Q.Insert(4);
+            Q.Insert(1);
+            Q.Insert(9);
+            Q.Insert(4);
+            Console.WriteLine("Q:"+Q);
+            Console.WriteLine("Sum:"+SumQ(Q));
+            Console.WriteLine("Max:"+MaxQ(Q));
+            Console.WriteLine("Even Queue:"+EvenCopy(Q));
+            MoveMax2Last(Q);
+            Console.WriteLine("Moved Max To Last:"+Q);
+        }
+    }
+}
---
+++ BasicQueue/BasicQueue/Queue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicQueue
+{
+    class Queue<T>
+    {
+        private Node<T> first;
+
+        private Node<T> last;
+        //-----------------------------------
+        public Queue()
+        {
+            this.first = null;
+
+            this.last = null;
+        }
+        //-----------------------------------
+        public bool IsEmpty()
+        {
+            return (this.first == null);
+        }
+        //-----------------------------------
+        public void Insert(T x)
+        {
+            Node<T> temp = new Node<T>(x);
+
+            if (this.first == null)
+
+                this.first = temp;
+            else
+
+                this.last.SetNext(temp);
+
+            this.last = temp;
+        }
+        //-------------------------------------
+        public T Remove()
+        {
+            T x = this.first.GetValue();
+
+            first = first.GetNext();
+
+            if (this.first == null)
+
+                this.last = null;
+
+            return (x);
+        }
+        //-------------------------------------
+        public T Head()
+        {
+            return (this.first.GetValue());
+        }
+
+        //-------------------------------------
+        public Queue<T> CopyQ()
+        {
+            Queue<T> Temp = new Queue<T>();
+            Node<T> pos = this.first;
+            while (pos!=null)
+            {
+                Temp.Insert(pos.GetValue());
+                pos = pos.GetNext();
+            }
+            return Temp;
+        }
+
+        //-------------------------------------
+        public override string ToString()
+        {
+            string st = "[";
+
+            Node<T> pos = this.first;
+
+            while (pos != null)
+            {
+                st += pos.GetValue();
+
+                if (pos.GetNext() != null)
+
+                    st += ",";
+
+                pos = pos.GetNext();
+            }
+            st += "]";
+            return (st);
+        }
+    }
+}
